ed7a0fba70faf576cc1d7bbcd8aecc9a789143cc
willmlee <100592713+willmlee@users.noreply.github.com>
2022-06-14 10:19:51 -0400

    Create scene2.js

---
+++ scene2.js
@@ -0,0 +1,9 @@
+class Scene2 extends Phaser.Scene {
+  constructor() {
+    super({key: 'scene2'})
+  }
+}
+
+/*function create() {
+  this.add.text(200, 200, 'New Scene', {fill: '#000000', fontSize: '20px'}) 
+} */
